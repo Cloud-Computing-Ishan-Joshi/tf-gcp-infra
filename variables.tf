56b6c5d7cb5fa8a172097f752c03ff55e1c16b25
Ishan Joshi <ishan25j@gmail.com>
2024-02-15 10:36:26 -0500

    Add Tf config

---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "project_id" {
+}
+
+variable "region" {
+}
+
+variable "vpc_name" {
+}
+
+variable "webapp_subnet_cidr" {
+}
+
+variable "db_subnet_cidr" {
+}
+
+variable "webapp_subnet_name" {
+}
+
+variable "db_subnet_name" {
+}
+
+# route
+variable "route_name" {
+}
+
+variable "route_dest_range" {
+}
+
+variable "zone" {
+}
+
+variable "credentials" {
+}
