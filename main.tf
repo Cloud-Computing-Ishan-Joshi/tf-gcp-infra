1a885ff4028aaaddb2e5e30022e8ace2ab61cc3e
Ishan Joshi <ishan25j@gmail.com>
2024-02-15 10:36:26 -0500

    Add Tf config

---
+++ main.tf
@@ -0,0 +1,28 @@
+resource "google_compute_network" "vpc" {
+  name                    = var.vpc_name
+  auto_create_subnetworks = false
+  routing_mode            = "REGIONAL"
+}
+
+resource "google_compute_subnetwork" "webapp" {
+  name          = var.webapp_subnet_name
+  ip_cidr_range = var.webapp_subnet_cidr
+  network       = google_compute_network.vpc.self_link
+  region        = var.region
+}
+
+resource "google_compute_subnetwork" "db" {
+  name          = var.db_subnet_name
+  ip_cidr_range = var.db_subnet_cidr
+  network       = google_compute_network.vpc.self_link
+  region        = var.region
+}
+
+resource "google_compute_route" "webapp" {
+  name        = var.route_name
+  dest_range  = var.route_dest_range
+  network     = google_compute_network.vpc.name
+  next_hop_gateway = "default-internet-gateway"
+  priority    = 1000
+  tags        = ["webapp"]
+}
