f6df20b4c3a1728480b24baccea57ddf5008acb2
Ishan Joshi <ishan25j@gmail.com>
2024-02-15 15:53:30 -0500

    multi vpc

--- variables.tf
+++ variables.tf
@@ -4,30 +4,11 @@ variable "project_id" {
 variable "region" {
 }
 
-variable "vpc_name" {
-}
 
-variable "webapp_subnet_cidr" {
-}
 
-variable "db_subnet_cidr" {
-}
-
-variable "webapp_subnet_name" {
-}
-
-variable "db_subnet_name" {
-}
-
-# route
-variable "route_name" {
-}
-
-variable "route_dest_range" {
+variable "credentials" {
 }
 
 variable "zone" {
-}
-
-variable "credentials" {
+  
 }
