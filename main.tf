01f8526df967f810d778b3ae6db0066c53c77088
ishan25j <ishan25j@gmail.com>
2024-02-26 22:43:22 -0500

    added config for cloud sql

--- main.tf
+++ main.tf
@@ -1,3 +1,178 @@
+
+resource "google_project_service" "service_networking" {
+  project = var.project_id
+  service = "servicenetworking.googleapis.com"
+}
+
+variable "vpcs" {
+  description = "List of VPCs"
+  type        = list(string)
+  default     = ["vpc1"]
+}
+
+resource "random_password" "db_password" {
+  length = 16
+  special = true
+}
+
+resource "random_id" "db_name_suffix" {
+  byte_length = 8
+}
+
+resource "google_compute_network" "vpc" {
+  for_each                        = toset(var.vpcs)
+  name                            = each.key
+  auto_create_subnetworks         = false
+  routing_mode                    = var.routing_mode
+  delete_default_routes_on_create = true
+
+  depends_on = [google_project_service.service_networking]
+}
+
+resource "google_compute_global_address" "private_ip" {
+  for_each = google_compute_network.vpc
+  name     = "${each.key}-private-ip"
+  purpose  = var.private_ip_purpose
+  ip_version   = var.ip_version
+  address_type = var.address_type
+  prefix_length = var.prefix_length
+  network  = each.value.self_link
+
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+  for_each = google_compute_network.vpc
+  network  = each.value.self_link
+  service  = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip[each.key].name]
+  
+  depends_on = [
+    google_compute_global_address.private_ip
+  ]
+}
+
+resource "google_compute_subnetwork" "webapp" {
+  for_each      = google_compute_network.vpc
+  name          = "${each.key}-webapp"
+  ip_cidr_range = var.cidr1
+  network       = each.value.self_link
+  region        = var.region
+}
+
+resource "google_compute_subnetwork" "db" {
+  for_each      = google_compute_network.vpc
+  name          = "${each.key}-db"
+  ip_cidr_range = var.cidr2
+  network       = each.value.self_link
+  region        = var.region
+}
+
+resource "google_compute_route" "webapp" {
+  for_each         = google_compute_network.vpc
+  name             = "${each.key}-route"
+  dest_range       = var.route_dest_range
+  network          = each.value.name
+  next_hop_gateway = "default-internet-gateway"
+  priority         = 1000
+  tags             = ["${each.key}-webapp"]
+}
+
+resource "google_sql_database_instance" "db_instance" {
+  for_each = google_compute_network.vpc
+  # random instance name
+  name     = "${each.key}-db-instance-${random_id.db_name_suffix.hex}"
+  region   = var.region
+  project  = var.project_id
+  database_version = var.db_version
+  depends_on = [google_service_networking_connection.private_vpc_connection]
+  settings {
+    tier = var.db_tier
+    deletion_protection_enabled = var.deletion_protection_enabled
+    availability_type = var.db_availability_type
+    disk_type = var.db_disk_type
+    disk_size = var.db_disk_size
+    ip_configuration {
+      ipv4_enabled = var.ipv4_enabled
+      private_network = google_compute_network.vpc[each.key].self_link
+      # enable_private_path_for_google_cloud_services = var.enable_private_path_for_google_cloud_services
+    }
+  }
+}
+
+
+resource "google_sql_database" "db" {
+  for_each = google_compute_network.vpc
+  name     = var.database_name
+  instance = google_sql_database_instance.db_instance[each.key].name
+}
+
+resource "google_sql_user" "db_user" {
+  for_each = google_compute_network.vpc
+  name     = var.db_user_name
+  instance = google_sql_database_instance.db_instance[each.key].name
+  password = random_password.db_password.result
+}
+
+# Create a VM instance with custom image
+resource "google_compute_instance" "vm_instance_webapp" {
+  for_each     = google_compute_network.vpc
+  name         = var.vm_instance_name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    # Use Custom Image
+    initialize_params {
+      image = "projects/${var.project_id}/global/images/${var.custom_image_name}"
+      size  = var.image_size
+      type  = var.image_type
+    }
+  }
+
+  network_interface {
+    # Use custom VPC and Subnet for network interface
+    network    = each.value.self_link
+    subnetwork = google_compute_subnetwork.webapp[each.key].self_link
+    access_config {
+      // Ephemeral IP
+
+    }
+  }
+  metadata = {
+    # Add startup script
+    # startup-script = <<-SCRIPT
+    # #! /bin/bash
+    
+    # # Generate .env file with database details from Terraform outputs
+    # # mkdir -p /var/env_webapp
+    # # check if .env exists, if not create it else dont overwrite it using echo > and >>
+    # # if [ ! -f /var/.env ]; then
+    # sudo touch /var/.env
+    # sudo echo "DB_HOST=${google_sql_database_instance.db_instance[each.key].private_ip_address}" | sudo tee -a /var/.env
+    # sudo echo "DB_USER=${google_sql_user.db_user[each.key].name}" | sudo tee -a /var/.env
+    # sudo echo "DB_PASSWORD=${google_sql_user.db_user[each.key].password}" | sudo tee -a /var/.env
+    # sudo echo "DB_NAME=${google_sql_database.db[each.key].name}" | sudo tee -a /var/.env
+    # sudo echo "NODE_ENV=test" | sudo tee -a /var/.env
+    # # fi
+
+    # sudo chmod 600 /var/.env
+    # sudo chown ${var.chown_user}:${var.chown_user} /var/.env
+    # # SCRIPT
+    # SCRIPT
+
+    startup-script = templatefile("startup_script.sh", {
+      db_host = google_sql_database_instance.db_instance[each.key].private_ip_address,
+      db_user = google_sql_user.db_user[each.key].name,
+      db_password = google_sql_user.db_user[each.key].password,
+      db_name = google_sql_database.db[each.key].name
+      chown_user = var.chown_user
+    })
+
+  } 
+  
+  tags = ["${each.key}-webapp", "http-server"]
+}
+
 resource "google_compute_firewall" "allow_http" {
 
   for_each = google_compute_network.vpc
@@ -37,70 +212,3 @@ resource "google_compute_firewall" "deny_all" {
 
 }
 
-variable "vpcs" {
-  description = "List of VPCs"
-  type        = list(string)
-  default     = ["vpc1"]
-}
-
-resource "google_compute_network" "vpc" {
-  for_each                        = toset(var.vpcs)
-  name                            = each.key
-  auto_create_subnetworks         = false
-  routing_mode                    = var.routing_mode
-  delete_default_routes_on_create = true
-}
-
-resource "google_compute_subnetwork" "webapp" {
-  for_each      = google_compute_network.vpc
-  name          = "${each.key}-webapp"
-  ip_cidr_range = var.cidr1
-  network       = each.value.self_link
-  region        = var.region
-}
-
-resource "google_compute_subnetwork" "db" {
-  for_each      = google_compute_network.vpc
-  name          = "${each.key}-db"
-  ip_cidr_range = var.cidr2
-  network       = each.value.self_link
-  region        = var.region
-}
-
-resource "google_compute_route" "webapp" {
-  for_each         = google_compute_network.vpc
-  name             = "${each.key}-route"
-  dest_range       = var.route_dest_range
-  network          = each.value.name
-  next_hop_gateway = "default-internet-gateway"
-  priority         = 1000
-  tags             = ["${each.key}-webapp"]
-}
-
-# Create a VM instance with custom image
-resource "google_compute_instance" "vm_instance_webapp" {
-  for_each     = google_compute_network.vpc
-  name         = var.vm_instance_name
-  machine_type = var.machine_type
-  zone         = var.zone
-
-  boot_disk {
-    # Use Custom Image
-    initialize_params {
-      image = "projects/${var.project_id}/global/images/${var.custom_image_name}"
-      size  = var.image_size
-      type  = var.image_type
-    }
-  }
-
-  network_interface {
-    # Use custom VPC and Subnet for network interface
-    network    = each.value.self_link
-    subnetwork = google_compute_subnetwork.webapp[each.key].self_link
-    access_config {
-      // Ephemeral IP
-
-    }
-  }
-  tags = ["${each.key}-webapp", "http-server"]
-}
--- variables.tf
+++ variables.tf
@@ -1,29 +1,47 @@
 variable "project_id" {}
+variable "credentials" {}
 
+# VPC configuration
 variable "region" {}
-
-# variable "credentials" {}
 
 variable "zone" {}
 
 variable "cidr1" {}
 variable "cidr2" {}
 
+variable "routing_mode" {
+  default = "REGIONAL"
+}
+
 variable "route_name" {}
 
 variable "route_dest_range" {}
+
+# Network configuration
+variable "private_ip_purpose" {
+  default = "VPC_PEERING"
+}
+
+variable "ip_version" {
+  default = "IPV4"
+}
+
+variable "address_type" {
+  default = "INTERNAL"
+}
+
+variable "prefix_length" {
+  default = 16
+}
+
+
+# VM Instance
 
 variable "vm_instance_name" {}
 
 variable "machine_type" {}
 
 variable "custom_image_name" {}
-
-variable "credentials" {}
-
-variable "routing_mode" {
-  default = "REGIONAL"
-}
 
 variable "image_size" {
   default = "100"
@@ -33,10 +51,58 @@ variable "image_type" {
   default = "pd-standard"
 }
 
+
+# Firewall configuration
 variable "firewall_deny" {
   default = ["22"]
 }
 
 variable "firewall_allow" {
   default = ["3000"]
+}
+
+# Database configuration
+variable "db_tier" {
+  default = "db-f1-micro"
+}
+
+variable "db_version" {
+  default = "POSTGRES_15"
+}
+
+variable "db_disk_size" {
+  default = "10"
+}
+
+variable "db_disk_type" {
+  default = "pd-ssd"
+}
+
+variable "db_availability_type" {
+  default = "REGIONAL"
+}
+
+variable "deletion_protection_enabled" {
+  default = false
+}
+
+variable "ipv4_enabled" {
+  default = false
+}
+
+variable "enable_private_path_for_google_cloud_services" {
+  default = true
+}
+
+variable "database_name" {
+  default = "webapp"
+}
+
+variable "db_user_name" {
+  default = "webapp"
+  sensitive = true
+}
+
+variable "chown_user" {
+  default = "csye6225"
 }
